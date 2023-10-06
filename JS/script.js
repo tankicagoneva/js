4e0360e8bbc7d96b935a2cde20cc7004c8a7b86b
Tankica Goneva <59257236+tankicagoneva@users.noreply.github.com>
2023-10-06 10:44:28 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,85 @@
+"use strict";
+
+// console.log("Hello World!");
+
+// console.log("Tankica Spasev"); //Tankica Spasev
+// console.log("1995"); //1195
+
+// console.log("Tankica Spasev", 1995); // Tankica Spasev 1995
+
+// console.log("Tankica" + "Spasev" + 1995); //TankicaSpasev11995
+
+// let name = "John";
+// let age = 25;
+
+// console.log("Name:", name);
+// console.log("Age:", age);
+
+// let student = name;
+
+/* let name = "Tankica";
+let surname = "Spasev";
+
+let age = 28; */
+
+// let fullName = name + " " + surname + " " + age;
+// console.log(fullName);
+
+// console.log("My name is", name, surname, "and I am", age, "years old.");
+
+// height = 180;
+// console.log(height);
+
+// console.log("Student:", student);
+// console.log("maxAge", maxAge);
+
+// let height;
+// let height;
+// console.log(height);
+
+// var height;
+// var height;
+// console.log(height);
+
+// let firstName = "John";
+// firstName = 1;
+
+// console.log(firstName);
+
+// let width = 100;
+// width = 200;
+
+// console.log(width);
+
+// let counter = 100;
+// let name = "John";
+// name = name + counter;
+// console.log(name);
+
+// const greeting = "Hello";
+// greeting = "Hi";
+
+// console.log(greeting);
+
+// let contact1Name = "John Doe";
+// let contact1Phone = "(555)555-1234";
+// let contact1Email = "john.doe@example.com";
+
+// let contact2Name = "Alice Johnson";
+// let contact2Phone = "(555)555-5678";
+// let contact2Email = "alice.johnson@example.com";
+
+// let contact3Name = "Bob Smith";
+// let contact3Phone = "(555)555-9876";
+// let contact3Email = "bob.smith@example.com";
+
+// console.log(contact1Name, "/", contact1Phone, "/", contact1Email);
+// console.log(contact3Name, "/", contact3Phone, "/", contact3Email);
+
+// let steps = 100;
+// console.log(typeof steps);
+// console.log(typeof contact1Phone);
+
+// let isString = true;
+
+// console.log(isString);
