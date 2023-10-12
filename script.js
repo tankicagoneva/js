5e1a0ac8afcfc6a1999768e29f61489cbdf85101
Tankica Goneva <59257236+tankicagoneva@users.noreply.github.com>
2023-10-12 20:30:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,331 @@
+// "use strict";
+
+// console.log("Hello World!");
+
+// console.log("Tankica Spasev"); //Tankica Spasev
+// console.log("1995"); //1195
+
+// console.log("Tankica Spasev", 1995); // Tankica Spasev 1995
+
+// console.log("Tankica" + "Spasev" + 1995); //TankicaSpasev11995
+
+// let name = "John";
+// let age = 25;
+
+// console.log("Name:", name);
+// console.log("Age:", age);
+
+// let student = name;
+
+/* let name = "Tankica";
+let surname = "Spasev";
+
+let age = 28; */
+
+// let fullName = name + " " + surname + " " + age;
+// console.log(fullName);
+
+// console.log("My name is", name, surname, "and I am", age, "years old.");
+
+// height = 180;
+// console.log(height);
+
+// console.log("Student:", student);
+// console.log("maxAge", maxAge);
+
+// let height;
+// let height;
+// console.log(height);
+
+// var height;
+// var height;
+// console.log(height);
+
+// let firstName = "John";
+// firstName = 1;
+
+// console.log(firstName);
+
+// let width = 100;
+// width = 200;
+
+// console.log(width);
+
+// let counter = 100;
+// let name = "John";
+// name = name + counter;
+// console.log(name);
+
+// const greeting = "Hello";
+// greeting = "Hi";
+
+// console.log(greeting);
+
+// let contact1Name = "John Doe";
+// let contact1Phone = "(555)555-1234";
+// let contact1Email = "john.doe@example.com";
+
+// let contact2Name = "Alice Johnson";
+// let contact2Phone = "(555)555-5678";
+// let contact2Email = "alice.johnson@example.com";
+
+// let contact3Name = "Bob Smith";
+// let contact3Phone = "(555)555-9876";
+// let contact3Email = "bob.smith@example.com";
+
+// console.log(contact1Name, "/", contact1Phone, "/", contact1Email);
+// console.log(contact3Name, "/", contact3Phone, "/", contact3Email);
+
+// let steps = 100;
+// console.log(typeof steps);
+// console.log(typeof contact1Phone);
+
+// let isString = true;
+
+// console.log(isString);
+
+// let counter = 100;
+// let counter = 200;
+// counter = 300;
+
+// console.log(counter);
+
+// let a = 10;
+// let b = 4;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+// let c = "dog";
+// console.log(a + c);
+
+// let d = "10";
+
+// let sum = a + d;
+// console.log(sum); //1010
+// console.log(typeof sum);
+
+// Math.round(40.5);
+// Math.floor(40.5);
+// Math.ceil(40.999);
+// Math.random();
+
+// let modulo = a % b;
+// console.log(modulo);
+
+// console.log(2 ** 4); //16
+// Math.pow(2, 4); // 16
+
+// console.log(20 / "steps"); //NaN - Not a number
+
+// let message = "The ship 'MARS' docked at the harbor";
+// let message1 = 'The ship "MARS" docked at the harbor';
+// let message2 = 'The ship "MARS" docked at the harbor';
+
+// let song =
+//   "This\
+// \
+// is \
+// \
+// a short\
+// \
+// message";
+
+// console.log(song);
+
+// let song1 = `This
+
+// is
+
+// a short
+
+// message`;
+
+// console.log(song1);
+
+// let name = "John";
+// console.log(`Hello, ${name}`);
+// console.log("Hello," + " " + name);
+
+// let test = "100" + "10";
+// console.log(test);
+
+// let test1 = "100" - "10";
+// console.log(test1);
+// console.log(typeof test1);
+
+// let steps;
+// console.log(steps);
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Smith",
+//   age: 30,
+// };
+
+// const person1 = {
+//   user1: {
+//     firstName: "John",
+//     lastName: "Smith",
+//     age: 30,
+//   },
+
+//   user2: {
+//     firstName: "Jane",
+//     lastName: "Smith",
+//     age: 30,
+//   },
+// };
+
+// console.log(person.user2.name);
+// const person = {};
+// person.name = "John";
+// person.surname = "Doe";
+// console.log(person);
+
+// ("use strict");
+// const prefix = "username_";
+// let userName = "Jack";
+// // const userName = "Adam";
+// let prefixedUserName;
+// //  const prefixedUserName;
+// userName = "John";
+// prefixedUserName = prefix + userName;
+// console.log(prefixedUserName /*+ prefixedUserName2*/);
+// // console.log(prefixedUserName2);
+
+// let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+// console.log(days[0]);
+
+// console.log(days[1]);
+
+// console.log(days[2]);
+
+// days[0] = "Sunday";
+// console.log(days[0]);
+
+// let emptyArr = [];
+// console.log(emptyArr[0]);
+
+// let names = [
+//   ["Olivia", "Smith", "Jane", "Doe"],
+//   ["Oliver", "Daniels", "Alice", "Green"],
+// ];
+
+// console.log(names[0]);
+// console.log(names[0][0]);
+// console.log(names[0][3]);
+
+// console.log(names[1]);
+// console.log(names[1][0]);
+// console.log(names[1][3]);
+
+// let femaleNames = names[0];
+// console.log("femaleNames", femaleNames[0]);
+
+// let users = [
+//   { name: "John Doe", age: 30, email: "john@example.com" },
+//   { name: "Jane Smith", age: 25, email: "jane@example.com" },
+//   { name: "Bob Johnson", age: 35, email: "bob@example.com" },
+// ];
+
+// console.log(users);
+// console.log(users[2].age);
+
+// let books = [
+//   { title: "Speaking JavaScript", author: "Axel Rauschmayer", pages: "460" },
+//   {
+//     title: "Programming JavaScript Applications",
+//     author: "Eric Elliott",
+//     pages: "254",
+//   },
+//   {
+//     title: "Understanding ECMAScript 6",
+//     author: "Nicholas C. Zakas",
+//     pages: "352",
+//   },
+// ];
+
+// console.log(
+//   "Titles: " + books[0].title + ", " + books[1].title + ", " + books[2].title,
+// );
+
+// console.log(
+//   "Author names: " +
+//     books[0].author +
+//     "," +
+//     books[1].author +
+//     "," +
+//     books[2].author,
+// );
+
+// console.log(
+//   "Pages number: " +
+//     books[0].pages +
+//     "," +
+//     books[1].pages +
+//     "," +
+//     books[2].pages,
+// );
+
+// let n1 = 10;
+// let n2 = 10;
+
+// let n3 = 20;
+// let n4 = 20;
+
+// console.log(n1); //10
+// console.log(n1++); //10
+// console.log(n1); //11
+
+// console.log(n2); //10
+// console.log(++n2); // 11
+// console.log(n2); //11
+
+// console.log(n3); //20
+// console.log(n3++); //20
+// console.log(n3); //19
+
+// console.log(n4); //20
+// console.log(--n4); // 19
+// console.log(n4); //19
+
+// let x = 10;
+
+// x += 2;
+// x = x + 2;
+
+// let fullName = "Tankica";
+// fullName += "Spasev";
+// fullName += 28;
+
+// console.log(fullName); //"Tankica Spasev 28"
+
+// console.log(true && true); // AND
+// console.log(true || true); // OR
+// console.log(!true); // NOT - false
+// console.log(!false); // NOT - true
+
+// console.log(true || false); // true
+// console.log(false || !false); // true
+// console.log((false && false) || true); // true
+// console.log(true || false || (false && true)); //true
+
+// console.log(10 !== 5); // false
+// console.log(10 == 10); //true
+// console.log(10 === 10); //true
+// console.log(10 === "10"); //false
+
+// >=
+// <=
+
+// alert("Hello World");
+// window.alert("Random text");
+
+// let decision = window.confirm("Is is OK");
+// console.log(decision);
+
+// let name = window.prompt("What is your name");
+// console.log(name);
