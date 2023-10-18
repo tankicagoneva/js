a0545e74a83f4ec3742558cc8a0149013fefa926
Tankica Goneva <59257236+tankicagoneva@users.noreply.github.com>
2023-10-18 08:37:45 +0100

    homework

---
+++ homework.js
@@ -0,0 +1,24 @@
+// day3
+
+// 1.
+
+      // console.log(2 * 3 + 1); // expected 7
+      // console.log(2 ** 4); // expected 16
+      // console.log(5 * 1); // expected 5
+      // console.log(8 ** 2 - 5 ** 2); // expected 39
+
+// 2. 
+
+      // console.log(4 * 5 == 20);
+      // console.log(6 * 5 == "30");
+      // console.log(-17 < 0);
+      // console.log(25 > 1);
+      // console.log(2 + 2 > 2 - 4);
+
+// 3.
+
+      // let width = prompt("Volume of the box, enter width", 0);
+      // let height = prompt("Volume of the box, enter height", 0);
+      // let length = prompt("Volume of the box, enter length", 0);
+      // let volume = width * height * length;
+      // alert(`Calculated box volume is ${volume}`);
